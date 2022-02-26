3575b79a60571869c7828d70397d258abdd6dd61
Khü <jeanmarie.huchet.info@gmail.com>
2022-02-26 16:56:09 +0100

    Navigation 0.1

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./styles/");
+  eleventyConfig.addPassthroughCopy("./styles/");
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  
+  eleventyConfig.addFilter("linkActive", function(itemUrl, pageUrl) {
+    let response = '';
+
+    if (itemUrl === pageUrl) {
+      response += ' aria-current="page"';
+    }
+
+    if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+      response += ' data-state="active"';
+    }
+
+    return response;
   });
 
   return {
