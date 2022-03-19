b7d5c91ec7092b6f11f18013e167592a89eaf8df
Khü <jeanmarie.huchet.info@gmail.com>
2022-03-19 02:18:15 +0100

    Ajout de contenu en Page d'accueil

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const now = String(Date.now());
 
+// TOML - data format
+const toml = require("toml");
+
 // utils
 const { isCurrentPage, linkActived } = require("./src/_11ty/utils/page");
-// const { client } = require("./src/_11ty/utils/apiPrismic");
 
 // Prismic
 const { pluginPrismic } = require("eleventy-plugin-prismic");
@@ -14,6 +16,9 @@ const { in_uids } = require("./src/_11ty/tags/in_uids");
 module.exports = function (eleventyConfig) {
     // Layouts
     eleventyConfig.addLayoutAlias('default', 'layouts/default.liquid');
+
+    // TOML
+    eleventyConfig.addDataExtension("toml", contents => toml.parse(contents));
 
     // Utils
     eleventyConfig.addFilter("isCurrentPage", isCurrentPage);
